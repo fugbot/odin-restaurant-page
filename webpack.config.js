be438e41b16314bbf7bbc3b759c28d6388947798
fugbot <18075133+fugbot@users.noreply.github.com>
2024-07-19 13:49:42 +0000

    Remove node_modules

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',    
+        print: './src/print.js',
+
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+    }),
+    ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
