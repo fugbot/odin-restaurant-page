079aa3bd4e09112e02d8654ae79c57359b4e08c1
fugbot <18075133+fugbot@users.noreply.github.com>
2024-08-07 13:13:18 +0000

    Fix webpack js updating

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-
-console.log("onload");
-console.log("hello jan")
-
+import dumpsIcon from './dumps.jpg';
 
 const content = document.querySelector("#content");
 
 const title = document.createElement("h1");
-let image = document.createElement("img");
 const text = document.createElement("div");
 
-function hello() {
+function initialPage() {
     console.log("helo funct")
     content.appendChild(title);
     title.innerHTML += "Restaurant name";
 
-    image.src="<%=require('./dumps.jpg')%>";
-    content.appendChild(image);
+    const picture = new Image();
+    picture.src = dumpsIcon;
+    content.appendChild(picture);
     
     content.appendChild(text);
     text.innerHTML += "Food is Delicious. Eat me!";
 }
 
 window.onload = function() {
-    hello();
-    
+    initialPage();   
 }
 
-
-  
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  watch: true,
+  mode: 'development',
   module: {
     rules: [
       {
