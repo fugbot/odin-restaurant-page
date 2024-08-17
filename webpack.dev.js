e4c8f4703334a87eb68edaada7930f2521b8ac7e
fugbot <18075133+fugbot@users.noreply.github.com>
2024-08-17 09:50:50 +0200

    Update config files

---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    menu: "./src/menu.js",
+    contact: "./src/contact.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Custom template",
+      template: "src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  mode: "dev",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
